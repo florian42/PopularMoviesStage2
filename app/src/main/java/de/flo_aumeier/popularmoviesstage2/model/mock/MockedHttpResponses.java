68a455681e13b79198476cb33ab564df6b76349c
Florian Aumeier <flo.aumeier@gmail.com>
2017-03-10 20:49:07 +0100

    test: TmdbAPIEndpointIntInterfaceTest, MockedHttpResponses
    Implement JUnit test for TmdbAPIEndpointIntInterfaceTest. Added Class to hold constants for the JSON Responses.

---
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage2/model/mock/MockedHttpResponses.java
@@ -0,0 +1,34 @@
+package de.flo_aumeier.popularmoviesstage2.model.mock;
+
+/**
+ * Created by Flo on 10.03.2017.
+ */
+
+public class MockedHttpResponses {
+    public static final String GET_TRAILERS_FOR_MOVIE_JSON_RESPONSE = "{\n" +
+            "    \"id\": 263115,\n" +
+            "    \"results\": [\n" +
+            "        {\n" +
+            "            \"id\": \"58beab2dc3a368668f05311e\",\n" +
+            "            \"iso_639_1\": \"en\",\n" +
+            "            \"iso_3166_1\": \"US\",\n" +
+            "            \"key\": \"7aiUAaMbR64\",\n" +
+            "            \"name\": \"Official Trailer 2\",\n" +
+            "            \"site\": \"YouTube\",\n" +
+            "            \"size\": 1080,\n" +
+            "            \"type\": \"Trailer\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": \"58beab3bc3a3680db1013829\",\n" +
+            "            \"iso_639_1\": \"en\",\n" +
+            "            \"iso_3166_1\": \"US\",\n" +
+            "            \"key\": \"ot2X367CtO4\",\n" +
+            "            \"name\": \"Official Trailer 1\",\n" +
+            "            \"site\": \"YouTube\",\n" +
+            "            \"size\": 1080,\n" +
+            "            \"type\": \"Trailer\"\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}";
+
+}
--- app/src/test/java/de/flo_aumeier/popularmoviesstage2/model/TmdbApiEndpointInterfaceTest.java
+++ app/src/test/java/de/flo_aumeier/popularmoviesstage2/model/TmdbApiEndpointInterfaceTest.java
@@ -1,14 +1,41 @@
 package de.flo_aumeier.popularmoviesstage2.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+
+import de.flo_aumeier.popularmoviesstage2.model.mock.MockedHttpResponses;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by faumeier on 10.03.2017.
  */
 public class TmdbApiEndpointInterfaceTest {
-    @Mock
-    TmdbApiEndpointInterface apiEndpointInterface;
+
+    MockWebServer mockWebServer;
+    Retrofit mRetrofit;
+    TmdbApiEndpointInterface mTmdbApiEndpointInterface;
+
+    @Before
+    public void setUp() throws Exception {
+        mockWebServer = new MockWebServer();
+        Gson gson = new GsonBuilder().setLenient().create();
+        mRetrofit = new Retrofit.Builder().baseUrl(TmdbApiEndpointInterface.BASE_URL)
+                .addConverterFactory
+                        (GsonConverterFactory.create(gson))
+                .build();
+        mTmdbApiEndpointInterface = mRetrofit.create(TmdbApiEndpointInterface.class);
+    }
+
     @Test
     public void getPopularMoviesPage1() throws Exception {
 //TODO: Impelement
@@ -22,6 +49,16 @@ public class TmdbApiEndpointInterfaceTest {
     @Test
     public void getTrailersForMovie() throws Exception {
         //TODO: Impelement
+        final String movieId = "263115";
+        final String jsonResponseBody = MockedHttpResponses.GET_TRAILERS_FOR_MOVIE_JSON_RESPONSE;
+        mockWebServer.enqueue(new MockResponse().setBody(jsonResponseBody));
+        Call<ResultsTrailer> call = mTmdbApiEndpointInterface.getTrailersForMovie(movieId);
+        assertTrue(call.execute() != null);
     }
 
+    @After
+    public void tearDown() throws Exception {
+        mockWebServer.shutdown();
+
+    }
 }
