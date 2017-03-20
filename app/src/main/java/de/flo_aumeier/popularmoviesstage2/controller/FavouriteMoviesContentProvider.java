65c74071903fc7622c8382411dafc48288c87a07
Florian Aumeier <florian.aumeier@doubleSlash.de>
2017-03-20 17:17:38 +0100

    feat: Content Provider
    added class and added tag to android manifest file

---
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage2/controller/FavouriteMoviesContentProvider.java
@@ -0,0 +1,54 @@
+package de.flo_aumeier.popularmoviesstage2.controller;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import de.flo_aumeier.popularmoviesstage2.model.db.FavouriteMovieDbHelper;
+
+/**
+ * Created by faumeier on 20.03.2017.
+ */
+
+public class FavouriteMoviesContentProvider extends ContentProvider {
+    private FavouriteMovieDbHelper mFavouriteDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mFavouriteDbHelper = new FavouriteMovieDbHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
