d71f05d8ba42bed8eaedcd735b748d1919abf0ce
Florian Aumeier <florian.aumeier@doubleSlash.de>
2017-03-20 17:28:26 +0100

    feat: Content Provider
    added Uris to Contract

--- app/src/main/java/de/flo_aumeier/popularmoviesstage2/model/db/FavouriteMovieContract.java
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage2/model/db/FavouriteMovieContract.java
@@ -1,5 +1,6 @@
 package de.flo_aumeier.popularmoviesstage2.model.db;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,10 +8,18 @@ import android.provider.BaseColumns;
  */
 
 public class FavouriteMovieContract {
+    public static final String CONTENT_SCHEME = "content://";
+    public static final String AUTHORITY = "de.flo_aumeier.popularmoviesstage2";
+    public static final Uri BASE_CONTENT_URI = Uri.parse(CONTENT_SCHEME + AUTHORITY);
+    public static final String PATH_FAVOURITE_MOVIES = "movies";
+
+
     private FavouriteMovieContract() {
     }
 
     public static final class FavouriteMovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVOURITE_MOVIES).build();
+
         // COMPLETED (2) Inside create a static final members for the table name and each of the db columns
         public static final String TABLE_NAME = "movies";
         public static final String COLUMN_MOVIE_TITLE = "movieTitle";
