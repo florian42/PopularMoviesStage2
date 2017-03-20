ebe38debb43f9ebe185057f6e80f37d0d55017c1
Florian Aumeier <florian.aumeier@doubleSlash.de>
2017-03-20 17:42:05 +0100

    feat: Content Provider
    implemented insert and added field UriMatcher with method buildUriMatcher

--- app/src/main/java/de/flo_aumeier/popularmoviesstage2/controller/FavouriteMoviesContentProvider.java
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage2/controller/FavouriteMoviesContentProvider.java
@@ -1,13 +1,17 @@
 package de.flo_aumeier.popularmoviesstage2.controller;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import de.flo_aumeier.popularmoviesstage2.model.db.FavouriteMovieContract;
 import de.flo_aumeier.popularmoviesstage2.model.db.FavouriteMovieDbHelper;
 
 /**
@@ -15,7 +19,18 @@ import de.flo_aumeier.popularmoviesstage2.model.db.FavouriteMovieDbHelper;
  */
 
 public class FavouriteMoviesContentProvider extends ContentProvider {
+    public static final int MOVIES = 100;
+    public static final int MOVIES_WITH_ID = 101;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
     private FavouriteMovieDbHelper mFavouriteDbHelper;
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(FavouriteMovieContract.AUTHORITY, FavouriteMovieContract.PATH_FAVOURITE_MOVIES, MOVIES);
+        uriMatcher.addURI(FavouriteMovieContract.AUTHORITY, FavouriteMovieContract.PATH_FAVOURITE_MOVIES + "/#", MOVIES_WITH_ID);
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
@@ -39,7 +54,22 @@ public class FavouriteMoviesContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        final SQLiteDatabase db = mFavouriteDbHelper.getWritableDatabase();
+        Uri returnUri;
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MOVIES:
+                long id = db.insert(FavouriteMovieContract.FavouriteMovieEntry.TABLE_NAME, null, values);
+                if (id > 0) {
+                    returnUri = ContentUris.withAppendedId(FavouriteMovieContract.FavouriteMovieEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknow uri: " + uri);
+        }
+        return returnUri;
     }
 
     @Override
